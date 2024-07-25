bfb5b5a471367a8975d2bc186ead412ce985d72b
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-07-25 12:01:46 +0100

    fix pls

--- src/components/Draggable.jsx
+++ src/components/Draggable.jsx
@@ -14,14 +14,15 @@ export default function Draggable(props) {
     setObjects(groupRef.current.children);
   }, [groupRef]);
 
-  useEffect(() => {
-    controlsRef.current.addEventListener("hoveron", () => {
-      scene.orbitControls.enabled = false;
-    });
-    controlsRef.current.addEventListener("hoveroff", () => {
-      scene.orbitControls.enabled = true;
-    });
-  }, [objects]);
+  // useEffect(() => {
+  //   controlsRef.current.addEventListener("hoveron", () => {
+  //     scene.orbitControls.enabled = false;
+  //   });
+  //   controlsRef.current.addEventListener("hoveroff", () => {
+  //     scene.orbitControls.enabled = true;
+  //   });
+  // }, [objects]);
+
   return (
     <group ref={groupRef}>
       <dragControls ref={controlsRef} args={[objects, camera, gl.domElement]} />
